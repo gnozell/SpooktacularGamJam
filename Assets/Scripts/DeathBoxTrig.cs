fefc8acb88c89344b6c4ba6dd87ed4f6c8d2324e
Jequatot <jeffreyquattro@gmail.com>
2016-10-29 16:36:50 -0400

    Level 2 Fail & Success States

---
+++ Assets/Scripts/DeathBoxTrig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathBoxTrig : MonoBehaviour {
+
+    public GameObject sceneTrig;
+
+    void OnTriggerEnter(Collider Other)
+    {
+        sceneTrig.GetComponent<SceneManagerLevel2>().DeathIsTriggered = true;
+    }
+}
---
+++ Assets/Scripts/SceneManagerLevel2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManagerLevel2 : MonoBehaviour {
+
+    public bool WomanIsTriggered;
+    public bool DeathIsTriggered;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(WomanIsTriggered)
+        {
+            //win state
+        }
+        if (DeathIsTriggered)
+        {
+            //fail state
+        }
+	}
+}
