ccf11d676cdd79af05feb6d593a7c419ad0929a4
Jequatot <jeffreyquattro@gmail.com>
2016-10-28 20:31:33 -0400

    Some Sad Attempt at Animation
    # Conflicts:
    #	Assets/Art/dan-arm-l.png.meta
    #	Assets/Art/dan-arm-r.png.meta
    #	Assets/Art/dan-face-forward.png.meta
    #	Assets/Art/dan-foot-l.png.meta
    #	Assets/Art/dan-foot-r.png.meta
    #	Assets/Art/dan-leg-l.png.meta
    #	Assets/Art/dan-leg-r.png.meta
    #	Assets/Art/dan-thigh-l.png.meta
    #	Assets/Art/dan-thigh-r.png.meta
    #	Assets/Art/sire-and-lady-henchurt.png.meta

---
+++ Assets/Scripts/dan_anim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dan_anim : MonoBehaviour {
+
+    public GameObject Rarm;
+    public GameObject Rthigh;
+    public GameObject Rfoot;
+    public GameObject Larm;
+    public GameObject Lthigh;
+    public GameObject Lfoot;
+    public int speed;
+    bool limbSpinLeft;
+
+    // Use this for initialization
+    void Start () {
+        limbSpinLeft = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Rarm.transform.rotation = new Quaternion(Rarm.transform.rotation.w, Rarm.transform.rotation.x, Rarm.transform.rotation.y, Rarm.transform.rotation.z);
+	}
+}
