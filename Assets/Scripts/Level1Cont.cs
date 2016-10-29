9fea6e41ee902d4d1fe05ac36fcff500df84a5fb
Griffin Nozell <griffin.nozell@plexxi.com>
2016-10-29 15:10:39 -0400

    Level 1 Stubbbed

---
+++ Assets/Scripts/Level1Cont.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Cont : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Level1Cont2Manager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Cont2Manager : MonoBehaviour {
+
+	public GameObject daniel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		daniel.transform.position = new Vector3 (daniel.transform.position.x+5*Time.deltaTime,daniel.transform.position.y,daniel.transform.position.z);
+		
+	
+	}
+}
