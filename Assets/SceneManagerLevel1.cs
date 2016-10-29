fc36c5360bad56b7ca4be9818d2158375226b4d5
Griffin Nozell <griffin.nozell@plexxi.com>
2016-10-28 21:54:14 -0400

    Level 1 Progress

---
+++ Assets/SceneManagerLevel1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManagerLevel1 : MonoBehaviour {
+
+	public GameObject hill1;
+	public GameObject hill2;
+	public GameObject castlehill;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Bobbing.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Bobbing : MonoBehaviour {
+
+	public float bobspeed = 17f;
+	public bool directionbool = true;
+	public float floatStrength = 0.2f; 
+
+	float originalY;
+	float direction;
+
+	void Start()
+	{
+		this.originalY = this.transform.position.y;
+		if (directionbool) {
+			direction = 1f;
+		} else {
+			direction = -1f;
+		}
+	}
+
+	void Update()
+	{
+		transform.position = new Vector3(transform.position.x,
+			originalY + (direction*(float)Math.Sin(Time.time*bobspeed) * floatStrength),
+			transform.position.z);
+	}
+}
