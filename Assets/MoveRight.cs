419f8b2a0844b3e575a12af3d375a1ebc19ee5d1
Jequatot <jeffreyquattro@gmail.com>
2016-10-29 19:08:52 -0400

    Level 2 Cutscene

---
+++ Assets/MoveRight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MoveRight : MonoBehaviour {
+
+    public bool moveLeft = false;
+    float i;
+    public float startTime;
+    public float endTime = 100;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        i += Time.deltaTime;
+        if (i >= startTime)
+        {
+            if (moveLeft == false) transform.Translate(-.1f, 0, 0);
+            else transform.Translate(.1f, 0, 0);
+        }
+        if(i >= endTime)
+            SceneManager.LoadScene("Level 3");
+    }
+}
