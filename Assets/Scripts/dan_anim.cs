085829de60dfdf0afb2616614d78b16d5e05a505
Jequatot <jeffreyquattro@gmail.com>
2016-10-29 14:57:42 -0400

    Level 2 Basic
    He moves and there are buildings

--- Assets/Scripts/dan_anim.cs
+++ Assets/Scripts/dan_anim.cs
@@ -13,57 +13,94 @@ public class dan_anim : MonoBehaviour {
     public Sprite faceLeft;
     public Sprite faceFront;
     SpriteRenderer spriter;
+    Rigidbody rb;
 
-    float[] timers = new float[6];
-    int[] isGoingLeft = new int [] {1, -1, 1, -1, -1, 1};
+    public float maxX;
+    public float maxZ;
+    public float minX;
+    public float minZ;
+
+    float[] isGoingLeft = new float [] {1f, -1f, 1f, -1f, -1f, 1f};
     public float speed;
 
     // Use this for initialization
     void Start () {
         spriter = this.GetComponent<SpriteRenderer>();
+        rb = this.GetComponent<Rigidbody>();
         //limbSpinLeft = 1;
     }
 	
 	// Update is called once per frame
-	void Update () {
-        Larm.transform.Rotate(0, 0, isGoingLeft[0]);
-        Lthigh.transform.Rotate(0, 0, isGoingLeft[1]);
-        Lfoot.transform.Rotate(0, 0, isGoingLeft[2]);
-        Rarm.transform.Rotate(0, 0, isGoingLeft[3]);
-        Rthigh.transform.Rotate(0, 0, isGoingLeft[4]);
-        Rfoot.transform.Rotate(0, 0, isGoingLeft[5]);
+	void Update ()
+    {
+        speed = 1;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var j = Input.GetAxis("Jump") * Time.deltaTime * 150.0f;
+        
+        transform.Translate(x, 0, z);
 
-        if (speed < 0) {
+        if (this.transform.position.x > minX || this.transform.position.x < maxX) transform.Translate(x * -1, 0, 0);
+        if (this.transform.position.z > minZ || this.transform.position.z < maxZ) transform.Translate(0, 0, z * -1);
+        if (this.rb.velocity.y == 0) rb.AddForce(new Vector3(0, j, 0));
+        spriter.sprite = faceFront;
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
             spriter.sprite = faceLeft;
-        } else if (speed > 0) {
+        }
+        else if (Input.GetAxis("Horizontal") > 0f)
+        {
             spriter.sprite = faceRight;
         }
 
-        if (speed == 0) spriter.sprite = faceFront;
+        if (Larm.transform.rotation.z*360 >= 15) isGoingLeft[0] = -1f;
+        if (Larm.transform.rotation.z * 360 <= -45) isGoingLeft[0] = 1f;
 
-        if (timers[0] > 15) isGoingLeft[0] = -1;
-        if (timers[0] < -45) isGoingLeft[0] = 1;
+        if (Lthigh.transform.rotation.z * 360 >= 20) isGoingLeft[1] = -1f;
+        if (Lthigh.transform.rotation.z * 360 <= -20) isGoingLeft[1] = 1f;
 
-        if (timers[1] > 20) isGoingLeft[1] = -1;
-        if (timers[1] < -15) isGoingLeft[1] = 1;
+        if (Lfoot.transform.rotation.z * 360 >= 15) isGoingLeft[2] = -1f;
+        if (Lfoot.transform.rotation.z * 360 <= -45) isGoingLeft[2] = 1f;
 
-        if (timers[2] > 20) isGoingLeft[2] = -1;
-        if (timers[2] < -20) isGoingLeft[2] = 1;
+        if (Rarm.transform.rotation.z * 360 >= 45) isGoingLeft[3] = -1f;
+        if (Rarm.transform.rotation.z * 360 <= -15) isGoingLeft[3] = 1f;
 
-        if (timers[3] > 45) isGoingLeft[3] = -1;
-        if (timers[3] < -15) isGoingLeft[3] = 1;
+        if (Rthigh.transform.rotation.z * 360 >= 20) isGoingLeft[4] = -1f;
+        if (Rthigh.transform.rotation.z * 360 <= -20) isGoingLeft[4] = 1f;
 
-        if (timers[4] > 15) isGoingLeft[4] = -1;
-        if (timers[4] < -20) isGoingLeft[4] = 1;
+        if (Rfoot.transform.rotation.z * 360 >= 15) isGoingLeft[5] = -1f;
+        if (Rfoot.transform.rotation.z * 360 <= -45) isGoingLeft[5] = 1f;
 
-        if (timers[5] > 20) isGoingLeft[5] = -1;
-        if (timers[5] < -20) isGoingLeft[5] = 1;
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            Larm.transform.Rotate(0, 0, isGoingLeft[0]);
+            Lthigh.transform.Rotate(0, 0, isGoingLeft[1]);
+            Lfoot.transform.Rotate(0, 0, isGoingLeft[2]);
+            Rarm.transform.Rotate(0, 0, isGoingLeft[3]);
+            Rthigh.transform.Rotate(0, 0, isGoingLeft[4]);
+            Rfoot.transform.Rotate(0, 0, isGoingLeft[5]);
 
-        timers[0] += (float)isGoingLeft[0] * speed / 30f;
-        timers[1] += (float)isGoingLeft[1] * speed / 30f;
-        timers[2] += (float)isGoingLeft[2] * speed / 30f;
-        timers[3] += (float)isGoingLeft[3] * speed / 30f;
-        timers[4] += (float)isGoingLeft[4] * speed / 30f;
-        timers[5] += (float)isGoingLeft[5] * speed / 30f;
+            /*
+            if (timers[1] > 20) isGoingLeft[1] = -1;
+            if (timers[1] < -15) isGoingLeft[1] = 1;
+
+            if (timers[2] > 20) isGoingLeft[2] = -1;
+            if (timers[2] < -20) isGoingLeft[2] = 1;
+
+            if (timers[3] > 45) isGoingLeft[3] = -1;
+            if (timers[3] < -15) isGoingLeft[3] = 1;
+
+            if (timers[4] > 15) isGoingLeft[4] = -1;
+            if (timers[4] < -20) isGoingLeft[4] = 1;
+
+            if (timers[5] > 20) isGoingLeft[5] = -1;
+            if (timers[5] < -20) isGoingLeft[5] = 1;
+
+            timers[1] += (float)isGoingLeft[1] * speed * Time.deltaTime;
+            timers[2] += (float)isGoingLeft[2] * speed * Time.deltaTime;
+            timers[3] += (float)isGoingLeft[3] * speed * Time.deltaTime;
+            timers[4] += (float)isGoingLeft[4] * speed * Time.deltaTime;
+            timers[5] += (float)isGoingLeft[5] * speed * Time.deltaTime;*/
+        }
     }
 }
