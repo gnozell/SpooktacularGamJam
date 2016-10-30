343c837ee5f746880c4dae6e4ed8fe6af2d99abe
Alex-Aguilar <alexanderaguilar100@gmail.com>
2016-10-29 20:45:29 -0400

    Game Over Live
    fd

---
+++ Game Over Live/Assets/ExitPlease.cs
@@ -0,0 +1,19 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class ExitPlease : MonoBehaviour
+{
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
+
+
