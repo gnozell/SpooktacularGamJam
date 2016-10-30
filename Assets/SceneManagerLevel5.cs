8a588a2f1b820aacf425552587ecb59b09f15101
Jequatot <jeffreyquattro@gmail.com>
2016-10-30 17:11:32 -0400

    IT WORKS?????

--- Assets/SceneManagerLevel5.cs
+++ Assets/SceneManagerLevel5.cs
@@ -2,21 +2,23 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class SceneManagerLevel2 : MonoBehaviour {
+public class SceneManagerLevel5 : MonoBehaviour
+{
 
     public bool WomanIsTriggered;
     public bool DeathIsTriggered;
     public string where;
 
     // Use this for initialization
-    void Start () {
-        WomanIsTriggered = false;
-        DeathIsTriggered = false;
+    void Start()
+    {
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if(WomanIsTriggered)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (WomanIsTriggered)
         {
             SceneManager.LoadScene(where);
         }
@@ -24,5 +26,5 @@ public class SceneManagerLevel2 : MonoBehaviour {
         {
             SceneManager.LoadScene("GameOverMenu");
         }
-	}
+    }
 }
